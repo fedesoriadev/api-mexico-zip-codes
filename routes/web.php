fb529d9a8b2c535b29478cff439fbe8520b50a6c
Federico Soria <fedesoria.dev@gmail.com>
2022-05-19 14:00:22 -0300

    refactor: deactivate web routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\ZipCode;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +12,3 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
